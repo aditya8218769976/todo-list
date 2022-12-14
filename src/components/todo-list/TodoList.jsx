35fe67e8c36bc7982bf8c0e9e72bda33cbae53d7
Aditya <adi8218769976@gmail.com>
2022-12-14 23:39:32 +0530

    dnd functionality added

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -1,34 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../components/todo-list/Todolist.css";
+import { useRef } from "react";
 
 const TodoList = ({ todos, setTodos, setEditTodo }) => {
+  const [dragList, setDragList] = useState([todos]);
+
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const applyDragandDropHandler = (e) => {
+    const copyDragListItem = [...dragList];
+    const dragItemContent = copyDragListItem[dragItem.current];
+    copyDragListItem.splice(dragItem.current, 1);
+    copyDragListItem.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+
+    console.log(copyDragListItem, "copyDragListItem...");
+    setDragList(copyDragListItem);
+  };
+
   const editHandler = ({ id }) => {
     const findTodos = todos.find((item) => item.id === id);
     setEditTodo(findTodos);
   };
 
+  console.log("hello");
   const deleteHandler = ({ id }) => {
     setTodos(todos.filter((item) => item.id !== id));
   };
 
   return (
     <div className="todolist-container">
-      {todos.map((item) => (
-        <li className="todolist-li" key={item.id}>
-          <input
-            type="text"
-            value={item.title}
-            onChange={(e) => e.preventDefault()}
-          />
+      {dragList &&
+        dragList.map((item) => (
+          <li
+            className="todolist-li"
+            key={item.id}
+            onDragStart={(e) => dragStart(e, index)}
+            onDragEnter={(e) => dragEnter(e, index)}
+            onDragOver={(e) => e.preventDefault()}
+            draggable
+            onDragEnd={applyDragandDropHandler}
+          >
+            <input
+              type="text"
+              value={item.title}
+              onChange={(e) => e.preventDefault()}
+            />
 
-          <button onClick={() => editHandler(item)} className="btn-edit">
-            <i className="fa fa-pencil" aria-hidden="true"></i>
-          </button>
-          <button onClick={() => deleteHandler(item)} className="btn-delete">
-            <i className="fa-solid fa-trash"></i>
-          </button>
-        </li>
-      ))}
+            <button onClick={() => editHandler(item)} className="btn-edit">
+              <i className="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button onClick={() => deleteHandler(item)} className="btn-delete">
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </li>
+        ))}
     </div>
   );
 };
